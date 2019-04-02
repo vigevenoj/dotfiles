c81e5039b9cba83c17406ba8c53d414bb088203d
Jacob Vigeveno <vigevenoj@gmail.com>
2019-04-01 22:03:52 -0700

    upgrade some clj profile stuff

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,9 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-cljfmt "0.5.6"]
-                  [cider/cider-nrepl "0.14.0"]
-                  [venantius/ultra "0.5.1"]]
-        :dependencies [[slamhound "1.3.1"]]}
+{:user {:plugins [[lein-pprint "1.2.0"]
+                  [lein-cljfmt "0.6.4"]
+                  [lein-ancient "0.6.15"]
+                  [cider/cider-nrepl "0.21.1"]
+                  [venantius/ultra "0.6.0"]
+                  [org.tcrawley/dynapath "1.0.0"]
+                  ]
+        :dependencies [[slamhound "1.5.5"]]}
         :repl-options {:init (require 'cljfmt.core)}}
