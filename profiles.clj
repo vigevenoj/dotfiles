73446c40fb6c4c02a5892d0eb6e8be45a043745e
Jacob Vigeveno <304282+vigevenoj@users.noreply.github.com>
2020-02-22 20:50:21 -0800

    upgrades and cleanup
    newer cider-nrepl
    get rid of ultra because its dependencies don't play well with newer
    java

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [
                   [lein-ancient "0.6.15"]
-                  [cider/cider-nrepl "0.21.1"]
-                  [org.tcrawley/dynapath "1.0.0"]
+                  [cider/cider-nrepl "0.24.0"]
+                  [org.tcrawley/dynapath "1.1.0"]
+                  [lein-cljfmt  "0.6.6"]
                   ]
-        :dependencies []}
-        :repl-options {}}
+        :dependencies [[slamhound "1.5.5"]]}
+        :repl-options { :init (require 'cljfmt.core)}}
