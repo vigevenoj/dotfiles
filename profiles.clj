260b2d88c94acbdace03744e48a67e275bc7dc82
Jacob Vigeveno <304282+vigevenoj@users.noreply.github.com>
2020-02-22 20:49:30 -0800

    trying some stuff out

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,7 @@
-{:user {:plugins [[lein-pprint "1.2.0"]
-                  [lein-cljfmt "0.6.4"]
+{:user {:plugins [
                   [lein-ancient "0.6.15"]
                   [cider/cider-nrepl "0.21.1"]
-                  [venantius/ultra "0.6.0"]
                   [org.tcrawley/dynapath "1.0.0"]
                   ]
-        :dependencies [[slamhound "1.5.5"]]}
-        :repl-options {:init (require 'cljfmt.core)}}
+        :dependencies []}
+        :repl-options {}}
